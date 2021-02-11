d78146cd13a107426b38c650adf614931f4c82df
nikikoh <nikikoh5@gmail.com>
2021-02-11 23:29:38 +0900

    Revert "init: Postを実装するためにブランチを作成"
    This reverts commit 704b05f35bf662b16284b208a68dd82ce928ccb8.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,9 @@ class ApplicationController < ActionController::Base
     protected
   
     def configure_permitted_parameters
-      added_attrs = [ :email, :name, :password, :password_confirmation, :image ]
+      added_attrs = [ :email, :name, :password, :password_confirmation ]
       devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
       devise_parameter_sanitizer.permit :account_update, keys: added_attrs
       devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
-
-      # アカウントの画像を変更
-      devise_parameter_sanitizer.permit(:account_update, keys: %i(avatar))
     end 
 end
